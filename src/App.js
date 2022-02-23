2497f80e5c4fbff7a8a773b4a5f43a0d55010900
Prabhjyotg@gmail.com <prabhjyotg@gmail.com>
2022-02-23 13:47:18 -0500

    Added Chat Engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
-function App() {
+function App(){
   return (
-    <div className="app">
-      
-    </div>
+    <ChatEngine
+			height='100vh'
+			userName='alpha'
+			userSecret='rnL#U6Y5GxJjKcK'
+			projectID='c6998783-165a-4855-af39-e0d1faadeea5'
+		/>
   );
 }
 
